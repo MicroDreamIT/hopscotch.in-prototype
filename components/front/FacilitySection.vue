33f79cfd7558cccaa3c673e51b2bca3b18edef32
MicroDreamIT <shahanur700@gmail.com>
2019-05-12 17:21:09 +0600

    images add

--- components/front/FacilitySection.vue
+++ components/front/FacilitySection.vue
@@ -2,13 +2,32 @@
     <mdb-container>
         <mdb-row>
             <mdb-col col="4">
-                <mdb-icon icon="truck" />
+                <div>
+                    <mdb-icon icon="truck" />
+                </div>
+                <div>
+                    <p>FREE SHIPPING & RETURN</p>
+                    <p>Free shipping on all orders over $99.</p>
+                </div>
+
             </mdb-col>
             <mdb-col col="4">
-                <mdb-icon  icon="dollar-sign" />
+                <div>
+                    <mdb-icon icon="truck" />
+                </div>
+                <div>
+                    <p>FREE SHIPPING & RETURN</p>
+                    <p>Free shipping on all orders over $99.</p>
+                </div>
             </mdb-col>
             <mdb-col col="4">
-                <mdb-icon far icon="life-ring" />
+                <div>
+                    <mdb-icon icon="truck" />
+                </div>
+                <div>
+                    <p>FREE SHIPPING & RETURN</p>
+                    <p>Free shipping on all orders over $99.</p>
+                </div>
             </mdb-col>
         </mdb-row>
     </mdb-container>
